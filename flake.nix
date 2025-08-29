6ea0d3ad3fa1f2befb27c9a137e9924a6137c9bc
arcstur <arcstur@gmail.com>
2025-08-29 16:41:01 -0300

    dev: flake.nix for local development in NixOS

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        python312
+        python312Packages.requests
+        python312Packages.pandas
+        python312Packages.odfpy
+        python312Packages.xlrd
+      ];
+    };
+  };
+}
