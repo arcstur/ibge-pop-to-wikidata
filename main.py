405df620cad027caae06c672765b2a838d94f943
arcstur <arcstur@gmail.com>
2025-08-29 16:51:45 -0300

    feat: sort by QID

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from wikidata import EstimateToQs
 def main():
     logging.basicConfig(level="INFO")
     eqs = EstimateToQs()
+    full_qs_list = []
+    for estimate in ESTIMATE_YEARS:
+        estimate.download()
+        full_qs_list.extend(eqs.to_qs_list(estimate))
+    full_qs_list = sorted(full_qs_list)
     with open("result.qs", "w") as f:
-        for estimate in ESTIMATE_YEARS:
-            estimate.download()
-            qs_list = eqs.to_qs_list(estimate)
-            f.write("\n".join(qs_list) + "\n\n")
-    logging.info("QuickStatements command written to ./result.qs")
+        f.write("\n".join(full_qs_list) + "\n")
+    logging.info("QuickStatements command written to ./result.qs, sorted by QID")
 
 if __name__ == "__main__":
     main()
