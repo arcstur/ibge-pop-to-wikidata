e841b2ee91d088fdab6a69ba4cd69cbe28bc64cf
arcstur <arcstur@gmail.com>
2025-08-29 14:21:02 -0300

    refactor: fixing some parsing, including more years

--- estimates.py
+++ estimates.py
@@ -18,6 +18,9 @@ class Estimate:
         return f"./data/estimate/{self.date}.ods"
 
     def download(self):
+        if os.path.exists(self.path()):
+            logging.info(f"already downloaded: {self.path()}")
+            return
         logging.info(f"downloading {self.url}")
         response = requests.get(self.url)
         if self.url.endswith("zip"):
@@ -38,24 +41,30 @@ class Estimate:
 
     def df(self):
         if not hasattr(self, "_df"):
-            dtype = {
-                "COD. UF": str,
-                "COD. MUNIC": str,
-                "POPULAÇÃO ESTIMADA": int,
-            }
             df = pd.read_excel(
                 self.path(),
                 engine="odf",
-                usecols=list(dtype.keys()),
-                dtype=dtype,
+                dtype=str,
                 skiprows=self.skiprows,
                 skipfooter=self.skipfooter,
                 sheet_name=self.sheet_name,
                 thousands=",",
                 decimal=".",
             )
-            for col in dtype.keys():
+            df = df.rename(columns={" POPULAÇÃO ESTIMADA ": "POPULAÇÃO ESTIMADA"})
+            columns = ("COD. UF", "COD. MUNIC", "POPULAÇÃO ESTIMADA")
+            for col in columns:
                 assert col in df.columns, f"{col} not present in columns"
+            df = df[[*columns]]
+            df = df.dropna(subset=["POPULAÇÃO ESTIMADA"])
+            df["POPULAÇÃO ESTIMADA"] = (
+                df["POPULAÇÃO ESTIMADA"]
+                .str.replace(r"\(\d+\)", "", regex=True) # used for footnotes
+                .str.replace("(*)", "")  # used for footnotes
+                .str.replace(",", "")  # remove all decimal/thousand notation
+                .str.replace(".", "")
+                .astype(int)
+            )
             self._df = df
         return self._df
 
@@ -82,6 +91,28 @@ ESTIMATE_YEARS = [
         url="https://ftp.ibge.gov.br/Estimativas_de_Populacao/Estimativas_2025/estimativa_dou_2025.ods",
     ),
     Estimate(
+        date="2024-07-01",
+        url="https://ftp.ibge.gov.br/Estimativas_de_Populacao/Estimativas_2024/estimativa_dou_2024.ods",
+        sheet_name="MUNICÍPIOS",
+    ),
+    Estimate(
+        date="2021-07-01",
+        url="https://ftp.ibge.gov.br/Estimativas_de_Populacao/Estimativas_2021/estimativa_dou_2021.ods",
+    ),
+    Estimate(
+        date="2020-07-01",
+        url="https://ftp.ibge.gov.br/Estimativas_de_Populacao/Estimativas_2020/estimativa_dou_2020.ods",
+    ),
+    Estimate(
+        date="2019-07-01",
+        url="https://ftp.ibge.gov.br/Estimativas_de_Populacao/Estimativas_2019/estimativa_dou_2019.ods",
+    ),
+    Estimate(
+        date="2015-07-01",
+        url="https://ftp.ibge.gov.br/Estimativas_de_Populacao/Estimativas_2015/estimativa_dou_2015_20150915.ods",
+        skiprows=2,
+    ),
+    Estimate(
         date="1999-07-01",
         url="https://ftp.ibge.gov.br/Estimativas_de_Populacao/Estimativas_1999/estimativa_populacao_1999_ods.zip",
         skiprows=2,
@@ -94,4 +125,6 @@ def download_all():
     os.makedirs("./data/estimate", exist_ok=True)
     for estimate in ESTIMATE_YEARS:
         estimate.download()
-        logging.info(f"loaded {estimate.total_municipalities()} municipalities from {estimate.date}, total population = {estimate.total_population()}")
+        logging.info(
+            f"loaded {estimate.total_municipalities()} municipalities from {estimate.date}, total population = {estimate.total_population()}"
+        )
